8e85d6047bbc3857c5052631f151bf34d9e21023
Baizidmdashadzzaman <baizid.md.ashadzzaman@gmail.com>
2022-04-24 16:53:27 +0600

    first commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Laravel 9 Vue Vuex Counter Application
+
+![Image](1.PNG?raw=true "Image")
+
+![Image](2.PNG?raw=true "Image")
+
+![Image](3.PNG?raw=true "Image")
+
+![Image](4.PNG?raw=true "Image")
+
+![Image](5.PNG?raw=true "Image")
+
+### How Install
+
+You can simply run the project by following process
+
+```shell
+$ git clone https://github.com/Baizidmdashadzzaman/Laravel-9-Vue-Vuex-Counter-Application-.git
+```
+```shell
+$ cd Laravel-9-Vue-Vuex-Counter-Application-
+```
+```shell
+$ composer install
+```
+```shell
+$ npm install
+```
+```shell
+$ npm run watch
+```
+
+Import sql and set up .env file from sql folder. Rename .env.example to .env and run
+
+```shell
+$ php artisan key:generate
+```
+Then visit http://127.0.0.1:8000/ and enjoy.
+
+
+
+
---
+++ resources/js/router/index.js
@@ -0,0 +1,90 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import store from '../store'
+
+Vue.use(VueRouter)
+
+/* Guest Component */
+const Counter = () => import('../components/Counter.vue' /* webpackChunkName: "resource/js/components/login" */)
+const Login = () => import('../components/Login.vue' /* webpackChunkName: "resource/js/components/login" */)
+const Register = () => import('../components/Register.vue' /* webpackChunkName: "resource/js/components/register" */)
+/* Guest Component */
+
+/* Layouts */
+const DahboardLayout = () => import('../components/Layouts/Dashboard.vue' /* webpackChunkName: "resource/js/components/layouts/dashboard" */)
+/* Layouts */
+
+/* Authenticated Component */
+const Dashboard = () => import('../components/Dashboard.vue' /* webpackChunkName: "resource/js/components/dashboard" */)
+/* Authenticated Component */
+
+
+const Routes = [
+    {
+        name:"counter",
+        path:"/",
+        component:Counter,
+        meta:{
+            middleware:"guest",
+            title:`Counter`
+        }
+    },
+    {
+        name:"login",
+        path:"/login",
+        component:Login,
+        meta:{
+            middleware:"guest",
+            title:`Login`
+        }
+    },
+    {
+        name:"register",
+        path:"/register",
+        component:Register,
+        meta:{
+            middleware:"guest",
+            title:`Register`
+        }
+    },
+    {
+        path:"/dashboard",
+        component:DahboardLayout,
+        meta:{
+            middleware:"auth"
+        },
+        children:[
+            {
+                name:"dashboard",
+                path: '/dashboard',
+                component: Dashboard,
+                meta:{
+                    title:`Dashboard`
+                }
+            }
+        ]
+    }
+]
+
+var router  = new VueRouter({
+    mode: 'history',
+    routes: Routes
+})
+
+router.beforeEach((to, from, next) => {
+    document.title = `${to.meta.title} - ${process.env.MIX_APP_NAME}`
+    if(to.meta.middleware=="guest"){
+        if(store.state.auth.authenticated){
+            next({name:"dashboard"})
+        }
+        next()
+    }else{
+        if(store.state.auth.authenticated){
+            next()
+        }else{
+            next({name:"login"})
+        }
+    }
+})
+
+export default router
